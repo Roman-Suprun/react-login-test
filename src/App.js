271f6f8b38dd7ae73659ca051640c868a5cbb55b
Roman Suprun <supra8771@gmail.com>
2023-02-07 21:22:40 +0200

    Added axios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Link} from "react-router-dom";
 
-function App() {
+const App = () => {
     return (
         <div className="App">
             <header className="App-header">
--- src/UserInfoPage.js
+++ src/UserInfoPage.js
@@ -1,6 +1,17 @@
 import './App.css';
+import {useEffect} from "react";
+import axios from "axios";
 
-function UserInfoPage() {
+const UserInfoPage = () => {
+    const getInfo = async () => {
+        const res = await axios.post('http://localhost:4000/registration', {num1: 1, num2: 2});
+        console.log(res.data);
+    }
+
+    useEffect(() => {
+        getInfo()
+    }, []);
+
     return (
         <div className="App">
             User info page
